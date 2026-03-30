bce8a0c0b88b535811b4bd80cf9a3faaa4213054
nandinii103 <nandiniimaherey@gmail.com>
2026-03-30 13:04:08 +0100

    conditional statement

---
+++ conditional.py
@@ -0,0 +1,10 @@
+tempreture = int(input("Enter the tempreture:"))
+
+if tempreture >= 30:
+    print("It is warm,you can wear a t-shirt")
+elif tempreture >= 20:
+    print("The wheather is mild,wear long sleeves")
+elif tempreture >= 10:
+    print("It is cold outside wear a jacket")
+else:
+    print("It is very cold wear a hat and gloves")
