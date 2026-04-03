56dd42e2d01c1555abd38ccabae07375fbf43614
nandinii103 <nandiniimaherey@gmail.com>
2026-04-03 13:40:52 +0100

    bmichecker

--- bmichecker.py
+++ bmichecker.py
@@ -1,2 +1,18 @@
-height = float(input("Please enter your height:")
-weight = float(input("Please enter your ")
+height = float(input("Please enter your height in centimetres:"))
+weight = float(input("Please enter your weight in kilograms: "))
+
+height = height/100
+bmi = weight / (height * height)
+
+print("your BMI is:",bmi)
+if bmi <= 18.5:
+    print("You are underweight")
+elif bmi <= 25:
+    print("You are normal weight")
+elif bmi <= 65:
+    print("You are overweight")
+else:
+    print("You are obese")
+    
+
+
