52fece5a278457eaeb7b3539194040ea0d9a5537
nandinii103 <nandiniimaherey@gmail.com>
2026-04-06 21:50:32 +0100

    age

---
+++ age.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age > 10:
+    if age < 20:
+        print("Age is between 10 and 20")
+    else:
+        print("Age is greater than 20")
+else:
+       print("Age is less than 10")
