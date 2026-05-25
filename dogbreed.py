6f4391b9101b308400063b41d8d5651ec99a2062
nandinii103 <nandiniimaherey@gmail.com>
2026-05-25 14:02:15 +0100

    classes

---
+++ dogbreed.py
@@ -0,0 +1,21 @@
+class Dog():
+    species = "Dog"
+    def __init__(self,name,breed):
+         self.name = name
+         self.breed = breed
+dog1 = Dog("Simba", "German Shephard")
+dog2 = Dog("foxy" , "husky")
+
+print(dog1.name, "," ,dog1.breed,"," ,dog1.species)
+print(dog2.name, "," ,dog2.breed,"," ,dog2.species)
+
+        
+    
+
+
+
+
+
+    
+   
+   
